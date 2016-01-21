af9ae2efcc2bb8b02351cc857dfdc6b7e5c013ff
AFAgarap <abienfred.agarap@yahoo.com>
2016-01-21 12:02:30 +0800

    Not actually in the problem set of FEU, this one's from DOST ICTO [Constraint: n >= 0 && n <= 9]

---
+++ Java/AlphaNumeric.java
@@ -0,0 +1,13 @@
+public class AlphaNumeric{
+	public static void main(String[] args) {
+		String line = new java.util.Scanner(System.in).next();
+		for (int i = 0; i < line.length(); i++) {
+			if (Character.isDigit(line.charAt(i)) && !Character.isDigit(line.charAt(i + 1))){
+				for (int j = 0; j < Character.getNumericValue(line.charAt(i)); j++) {
+					System.out.print((line.charAt(i + 1)));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
