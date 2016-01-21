415136463b111579e2b55a68df494af00a566981
AFAgarap <abienfred.agarap@yahoo.com>
2016-01-21 13:48:39 +0800

    ArctanSeries solution

--- Java/ArctanSeries.java
+++ Java/ArctanSeries.java
@@ -3,19 +3,19 @@ public class ArctanSeries{
 		double x = new java.util.Scanner(System.in).nextDouble();
 		int k = new java.util.Scanner(System.in).nextInt();
 		double answer = x;
-		for(int i = 1, j = 1; i <= k; i++, j+=2){
+		for(int i = 1, j = 1; i < k; i++, j+=2){
 			if(i == 1){
 				answer -= Math.pow(x, (j + 2)) / (j + 2);
-				System.out.print("if: ");
+				// 0.5 - 0.5^3 / 3 = .458333
 			}else if(i % 2 == 0){
-				answer += Math.pow(x, j) / j;
-				System.out.print("else if (even): ");
+				answer += Math.pow(x, j + 2) / (j + 2);
+				// 0.458333 + 
 			}else if(i % 2 != 0){
-				answer -= Math.pow(x, j) / j;
+				answer -= Math.pow(x, j + 2) / (j + 2);
 				System.out.print("else if (odd): ");
 			}
 			System.out.println(answer);
-System.out.println("j: " + j);
+			System.out.println("j: " + j);
 		}
 		System.out.println("Answer: " + answer);
 	}
