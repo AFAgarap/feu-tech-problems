26d1919690ecf80891873504334ecf6e769e4549
AFAgarap <abienfred.agarap@yahoo.com>
2016-01-28 21:12:01 +0800

    Python solution for ArctanSeries problem

---
+++ Python/ArctanSeries.py
@@ -0,0 +1,14 @@
+def main():
+	x = input("Enter x: ")
+	k = input("Enter k: ")
+	answer = x; i = 1; j = 1
+	while(i < k):
+		if (i == 1):
+			answer -= (x ** (j + 2)) / (j + 2)
+		elif (i % 2 == 0):
+			answer += (x ** (j + 2)) / (j + 2)
+		elif (i % 2 != 0):
+			answer -= (x ** (j + 2)) / (j + 2)
+		i += 1; j += 2
+	print "Answer:", answer
+main()
