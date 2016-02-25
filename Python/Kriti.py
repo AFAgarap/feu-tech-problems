0adb15d4412dfb782c979ac72f76d97ca5ab2a27
AFAgarap <abienfred.agarap@yahoo.com>
2016-02-25 16:39:41 +0800

    Python solution for the Kriti problem from HackerEarth

---
+++ Python/Kriti.py
@@ -0,0 +1,25 @@
+number_string = int(input())
+
+strings = []
+
+for x in range(0, number_string):
+	strings.append(raw_input())
+
+number_query = int(input())
+
+count = []
+
+for x in range(0, number_query):
+	lrs = raw_input()
+	l, r, s = lrs.split()
+	l = int(l); r = int(r)
+	
+	counter = 0
+
+	for y in range((int(l) - 1), int(r)):
+		if (s == str(strings[y])):
+			counter += 1
+	count.append(counter)
+
+for x in range(len(count)):
+	print count[x]
