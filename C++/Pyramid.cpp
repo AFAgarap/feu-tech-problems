40175b5d15693e9d28349446575b39187be7a169
AFAgarap <abienfred.agarap@yahoo.com>
2016-03-03 21:57:18 +0800

    Number pyramid

---
+++ C++/Pyramid.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+	for (int i = 1; i <= 10; i++){
+		for (int k = 10; k >= 0; k--){
+			std::cout << ((k + i <= 10) ? " " : "");
+		}
+		for (int j = 1; j < i; j++){
+			std::cout << j << " ";
+		}
+		std::cout << "\n";		
+	}
+}
