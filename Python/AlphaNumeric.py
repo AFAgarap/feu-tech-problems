beb16f048c94baedfae622e22342186d7b29686b
AFAgarap <abienfred.agarap@yahoo.com>
2016-07-29 23:24:44 +0800

    Added comment

--- Python/AlphaNumeric.py
+++ Python/AlphaNumeric.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# Prints the given letter n times
+# Where n is the preceding number to the letter
 def main():
 	line = raw_input("Enter a string like 1a2b3c: ")
 	for x in range(len(line)):
