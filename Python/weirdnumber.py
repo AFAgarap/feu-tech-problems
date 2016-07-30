b4c5e6925ade014cc7e1cb6f1fb5e59d2eecf535
AFAgarap <abienfred.agarap@yahoo.com>
2016-07-30 21:51:09 +0800

    Added comment

--- Python/weirdnumber.py
+++ Python/weirdnumber.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python
+# If n is odd or if n is even and in between 6 and 20 (inclusive), print "Weird"
+# If n is even and in between 2 and 5 (inclusive) or if n is even and > 20, print "Not Weird"
 number = input('Enter a number:')
 print "Weird" if (number % 2 != 0 or number >= 5 and number <= 20) else "Not Weird"
