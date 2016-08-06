7122d3a6ab3fc824f089cc3a5431ff9b8d499c96
AFAgarap <abienfred.agarap@yahoo.com>
2016-08-07 00:11:25 +0800

    Reviewing Ruby programming by tutorial: BEGIN-END

---
+++ Ruby/begin-sample.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# This is a comment
+
+=begin
+This is a comment
+Multi-line comment
+=end
+
+puts "This is main Ruby program"
+
+END {
+	puts "Terminating Ruby program"
+}
+BEGIN {
+	puts "Initializing Ruby program"
+}
