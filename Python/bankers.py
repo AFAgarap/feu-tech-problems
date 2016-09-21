1b1e387b46cb6b6c8a642ded1ab3f150dd2ed4ea
AFAgarap <abienfred.agarap@yahoo.com>
2016-09-21 23:49:07 +0800

    Revised implementation of Banker's algorithm

--- Python/bankers.py
+++ Python/bankers.py
@@ -1,58 +1,118 @@
 #!/usr/bin/env python3
 
+from string import ascii_uppercase
+
+letters = list(ascii_uppercase)
+
 def main():
-	process = []
-	total_resources = int(input("Enter the amount of total resources: "))
-	size = int(input("How many processes will you enter? "))
+	available_instance = []
+	origin_available_instance = []
+	process = []	
+	total_allocation = []
+	total_resources = []
+
+	# f(x1)
+	number_of_resources = int(input("Enter number of resources: "))
+	size = int(input("Enter number of processes: "))
+
+	# f(x2)
+	total_resources = set_total_resources(number_of_resources=number_of_resources)
+
+	# f(x3)
+	process = set_processes(number_of_resources=number_of_resources, size=size)
+	total_allocation = get_total_allocation(process=process, size=len(process), number_of_resources=number_of_resources)
+	available_instance = get_available_instance(total_resources=total_resources, total_allocation=total_allocation, number_of_resources=number_of_resources)
+	origin_available_instance = list(available_instance)
 	
-	for index in range(0, size):
-		process.append([input("Enter maximum allocation for process #{}: ".format(index + 1)), input("Enter current allocation for process #{}: ".format(index + 1))])
+	# f(x4)
+	process = get_process_need(process=process, size=len(process), number_of_resources=number_of_resources)
 
-	difference = ""
-	for index in range(0, len(process)):
-		for char in range(0, len(process[index][0])):
-			difference += str(abs(int(process[index][0][char]) - int(process[index][1][char])))
-		process[index].append(difference)
-		difference = ""
-		
-	total_allocation = get_total_allocation(process=process)
+	temp = list(process)
 
-	origin_available_instance = get_available_instance(total_resources=total_resources, total_allocation=total_allocation)
-	available_instance = origin_available_instance
-
-	print("-----------------------------------------------------")
-	print("Max. Allocation\t\tAllocated Resource\tNeed")
-	for index in range(0, len(process)):
-		print("{}\t\t\t{}\t\t\t{}".format(process[index][0], process[index][1], process[index][2]))
-	print("-----------------------------------------------------")
-	
+	# f(x5)
 	while (len(process) != 0):
-		queue, process = compare_available_instance(process=process, available_instance=available_instance)
-		for index in range(0, len(queue)):
-			available_instance = (available_instance - int(queue[index][0])) + int(queue[index][1])
+		queue, process = compare_ai_need(available_instance=available_instance, process=process, size=len(process), number_of_resources=number_of_resources)
+		# f(x6)
+		available_instance = get_new_ai(available_instance=available_instance, queue=queue, size=len(queue), number_of_resources=number_of_resources)
+	
+	# f(x7)
+	is_safe, available_instance = check_safety(origin_available_instance=origin_available_instance, available_instance=available_instance, total_allocation=total_allocation, number_of_resources=number_of_resources)
+	
+	process = list(temp)
 
-	available_instance = available_instance - total_allocation
+	print("Total Resources : {}".format(total_resources))
+	print("Total Allocation : {}".format(total_allocation))
+	print("Original Available Instance : {}".format(origin_available_instance))
+	print("New Available Instance : {}".format(available_instance))
+	print("Processes : {}".format(process))
+	print("Safe" if is_safe == True else "Not Safe")
 
-	print("Safe state" if (available_instance == origin_available_instance) else "Not-safe state")
+def set_total_resources(number_of_resources=0):
+	total_resources = []
+	for resource_number in range(0, number_of_resources):
+		total_resources.append(int(input("Total Resources, value for {}: ".format(letters[resource_number]))))
+	return total_resources
 
-def compare_available_instance(process=[], available_instance=0):
-	queue = []; forRemoval = []
-	for index in range(0, len(process)):
-		if (int(process[index][2]) < available_instance):
-			queue.append([process[index][2], process[index][0]])
-			forRemoval.append(process[index])
-	queue.sort()
-	for index in range(0, len(forRemoval)):
-		process.remove(forRemoval[index])
-	return queue, process
+def set_processes(number_of_resources=0, size=0):
+	cur_allocation = []
+	max_allocation = []
+	process = []
+	for index in range(0, size):
+		for resource_number in range(0, number_of_resources):			
+			max_allocation.append(int(input("Max. Allocation #{}, value for {}: ".format(index + 1, letters[resource_number]))))
+		for resource_number in range(0, number_of_resources):			
+			cur_allocation.append(int(input("Cur. Allocation #{}, value for {}: ".format(index + 1, letters[resource_number]))))
+		process.append([max_allocation, cur_allocation])
+		max_allocation = []
+		cur_allocation = []
+	return process
 
-def get_available_instance(total_resources=0, total_allocation=0):
-	return total_resources - total_allocation
-
-def get_total_allocation(process=[]):
-	total_allocation = 0
-	for index in range(0, len(process)):
-		total_allocation += int(process[index][1])
+def get_total_allocation(process=[], size=0, number_of_resources=0):
+	summation = 0
+	total_allocation = []
+	for resource_number in range(0, number_of_resources):
+		for index in range(0, size):
+			summation += process[index][1][resource_number]
+		total_allocation.append(summation)
+		summation = 0
 	return total_allocation
 
-main()
+def get_available_instance(total_resources=[], total_allocation=[], number_of_resources=0):
+	available_instance = []
+	for resource_number in range(0, number_of_resources):
+		available_instance.append(total_resources[resource_number] - total_allocation[resource_number])
+	return available_instance
+
+def get_process_need(process=[], size=0, number_of_resources=0):
+	need = []
+	for index in range(0, size):
+		for resource_number in range(0, number_of_resources):
+			need.append(process[index][0][resource_number] - process[index][1][resource_number])
+		process[index].append(need)
+		need = []
+	return process
+
+def compare_ai_need(available_instance=[], process=[], size=0, number_of_resources=0):
+	for_removal = []
+	queue = []
+	for index in range(0, size):
+		if (process[index][2] < available_instance):
+			queue.append([process[index][2], process[index][0]])
+			for_removal.append(process[index])
+	queue.sort()
+	for index in range(0, len(for_removal)):
+		process.remove(for_removal[index])
+	return queue, process
+
+def get_new_ai(available_instance=[], queue=[], size=0, number_of_resources=0):
+	for index in range(0, size):
+		for resource_number in range(0, number_of_resources):
+			available_instance[resource_number] = abs(available_instance[resource_number] - queue[index][0][resource_number]) + queue[index][1][resource_number]
+	return available_instance
+
+def check_safety(origin_available_instance=[], available_instance=[], total_allocation=[], number_of_resources=0):
+	available_instance = list([abs(rn_available_instance - rn_total_allocation) for rn_available_instance, rn_total_allocation in zip(available_instance, total_allocation)])
+	return (True if (origin_available_instance == available_instance) else False), available_instance
+
+if __name__ == '__main__':
+	main()
