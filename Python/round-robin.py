2a9f2ad4493b6252569b2926eb89905fa997e500
AFAgarap <abienfred.agarap@yahoo.com>
2016-11-24 20:51:57 +0800

    Only issue now is the starting time

--- Python/round-robin.py
+++ Python/round-robin.py
@@ -8,10 +8,11 @@ def main():
     # size = int(input("How many processes will you enter? "))
     # process = set_processes(size)
     quantum = 2
+    size = 4
     process = [ [7, 9], [4, 4], [6, 3], [1, 2] ]
     process.sort()
     cycle = get_cycle(process[-1][1], quantum)
-    print(get_queue(process=process, cycle=cycle, quantum=quantum))
+    print(get_queue(process=process, cycle=cycle, quantum=quantum, size=size))
 
 def set_processes(size):
     process = []
@@ -26,29 +27,45 @@ def get_cycle(process, quantum):
     cycle = math.floor((process / quantum) + (process % quantum))
     return cycle
 
-def get_queue(process, cycle, quantum):
+def get_queue(process, cycle, quantum, size):
     queue = []
     for index in range(0, cycle):
         for element in range(0, len(process)):
             if not queue:
-                queue.append(process[element][0])
-                result = get_finished_time(burst_time=process[element][1], quantum=quantum, starting_time=queue[element])
-                process[element][1], finished_time = result[0], result[1]
-                queue[element] = [queue[element], finished_time]
-            elif queue:
+                # set_starting_time(queue, process[element][0])
+                starting_time = process[element][0]
+            elif queue and queue.__len__() < size:
                 if (process[element][0] <= queue[element - 1][1]):
-                    queue.append(queue[element - 1][1])
-                    result = get_finished_time(burst_time=process[element][1], quantum=quantum, starting_time=queue[element])
-                    process[element][1], finished_time = result[0], result[1]
-                    queue[element] = [queue[element], finished_time]
+                	# set_starting_time(queue, queue[element - 1][1])
+                	starting_time = queue[element - 1][1]
                 elif (process[element][0] > queue[element - 1][1]):
-                    queue.append(process[element][0])
-                    result = get_finished_time(burst_time=process[element][1], quantum=quantum, starting_time=queue[element])
-                    process[element][1], finished_time = result[0], result[1]
-                    queue[element] = [queue[element], finished_time]
+                	# set_starting_time(queue, process[element][0])
+                	starting_time = process[element][0]
+            elif queue and queue.__len__() >= size:
+            	starting_time = queue[-1][1] if index == 1 else queue[element - 1][1]
+            	print("e:",element)
+            	if process[element][1] == 0:
+            		continue
+            	elif process[element][1] != 0:
+            		set_starting_time(queue, starting_time)
+            # result = get_finished_time(burst_time=process[element][1], quantum=quantum, starting_time=queue[element])
+            result = get_finished_time(burst_time=process[element][1], quantum=quantum, starting_time=starting_time)
+            process[element][1], finished_time = result[0], result[1]
+            # queue[element] = [queue[element], finished_time]
+            
+            queue.append([starting_time, finished_time])
+            print(process)
+            print(queue)
+            print()
     return process, queue
 
+def set_starting_time(queue, starting_time):
+	queue.append(starting_time)
+	return queue
+
 def get_finished_time(burst_time, quantum, starting_time):
+    finished_time = 0
+    print("s:",starting_time)
     if burst_time >= quantum:
         finished_time = starting_time + quantum
         burst_time -= quantum
