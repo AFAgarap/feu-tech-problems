ad0acced37c6686430abb29f88b2f9a61f9698b9
AFAgarap <abienfred.agarap@yahoo.com>
2017-01-23 13:13:12 +0800

    Corrected the error on trying 0-10 to 0-9

--- Python/simple-bruteforce.py
+++ Python/simple-bruteforce.py
@@ -2,10 +2,10 @@ import os
 import signal
 import sys
 
-for w in range(0, 11):
-	for x in range(0, 11):
-		for y in range(0, 11):
-			for z in range(0, 11):
+for w in range(0, 10):
+	for x in range(0, 10):
+		for y in range(0, 10):
+			for z in range(0, 10):
 				if os.system('echo "{}" | gpg --passphrase-fd 0 {}'.format((str(w) + str(x) + str(y) + str(z)), sys.argv[1])) == 0:
 					os.system('figlet Cracked!')
 					print("The passphrase was {}".format(str(w) + str(x) + str(y) + str(z)))
