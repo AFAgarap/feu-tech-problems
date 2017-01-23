2e2ff0d65d9e9ccde6ffa6b8a1307a7cc8ba528b
AFAgarap <abienfred.agarap@yahoo.com>
2017-01-23 13:03:21 +0800

    Simple program for bruteforce

---
+++ Python/simple-bruteforce.py
@@ -0,0 +1,15 @@
+import os
+import signal
+import sys
+
+for w in range(0, 11):
+	for x in range(0, 11):
+		for y in range(0, 11):
+			for z in range(0, 11):
+				if os.system('echo "{}" | gpg --passphrase-fd 0 {}'.format((str(w) + str(x) + str(y) + str(z)), sys.argv[1])) == 0:
+					os.system('figlet Cracked!')
+					print("The passphrase was {}".format(str(w) + str(x) + str(y) + str(z)))
+					print()
+					os.kill(os.getpid(), signal.SIGINT)
+				else:
+					print("Cracking... {}".format(str(w) + str(x) + str(y) + str(z)))
