e55d6f38d7c09401da4d18917ec10a44678466e7
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-06 17:21:20 +0800

    No need to write in a main(), revised the docstring

--- Python/AlphaNumeric.py
+++ Python/AlphaNumeric.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-# Prints the given letter n times
-# Where n is the preceding number to the letter
-def main():
-	line = raw_input("Enter a string like 1a2b3c: ")
-	for x in range(len(line)):
-		if (line[x].isdigit()) and (not line[x + 1].isdigit()):
-			for y in range(0, int(line[x])):
-				print line[x + 1]
 
-main()
+"""
+Prints the given letter n times,
+where n is the preceding number to the letter.
+"""
+
+line = raw_input("Enter a string like 1a2b3c: ")
+for x in range(len(line)):
+	if (line[x].isdigit()) and (not line[x + 1].isdigit()):
+		for y in range(0, int(line[x])):
+			print line[x + 1]
